00854c5aab2fe05a2bf13a67b8ba030fa9080c26
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-11-30 00:28:50 +0200

    Stripped away old testing code

--- show.py
+++ show.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 from sdarot import Sdarot
-from threading import Thread
-from time import sleep
 
 
 def download(show, season, episode):
@@ -18,27 +13,4 @@ def download(show, season, episode):
 
 show = "money heist"
 
-proc = download(show, season=1, episode=1)
-exit(1)
-
-procs = []
-season = 1
-for episode in range(7, 14):
-    print("Downloading S{se}E{ep}...".format(se=season, ep=episode))
-    proc = Thread(target=download, args=[show, season, episode])
-    proc.setDaemon(True)
-    proc.start()
-    procs.append(proc)
-
-# procs = []
-# for season in range(1, 4 + 1):
-# 	for episode in range(1, 13 + 1):
-# 		print("Downloading S{se}E{ep}...".format(se = season, ep = episode))
-# 		proc = Thread(target = download, args = [show, season, episode])
-# 		proc.setDaemon(True)
-# 		proc.start()
-# 		procs.append(proc)
-# 		sleep(5)
-print("Working...")
-for proc in procs:
-    proc.join()
+download(show, season=1, episode=1)
